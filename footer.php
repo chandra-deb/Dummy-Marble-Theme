79149dcc19bbb97ef8b737c3298e6d0e48016c3b
chandra-deb <mail.chandradeb@gmail.com>
2021-02-19 22:54:00 +0600

    seperate header and footer

---
+++ footer.php
@@ -0,0 +1,31 @@
+</div>
+
+<!-- jQuery -->
+<script src="<?php echo get_theme_file_uri(
+    '/js/jquery.min.js' );
+?>"></script>
+<!-- jQuery Easing -->
+<script src="<?php echo get_theme_file_uri(
+    '/js/jquery.easing.1.3.js' );
+?>"></script> <!-- Bootstrap -->
+<script src="<?php echo get_theme_file_uri(
+    '/js/bootstrap.min.js' );
+?>"></script>
+<!-- Waypoints -->
+<script src="<?php echo get_theme_file_uri(
+    '/js/jquery.waypoints.min.js' );
+?>"></script>
+<!-- Flexslider -->
+<script src="<?php echo get_theme_file_uri(
+    '/js/jquery.flexslider-min.js' );
+?>"></script>
+
+
+<!-- MAIN JS -->
+<script src="<?php echo get_theme_file_uri(
+    '/js/main.js' );
+?>"></script>
+<?wp_footer(  ) ?>
+</body>
+
+</html>
